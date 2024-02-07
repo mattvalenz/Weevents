7410e9da94863086b866847e5dded4ae212f261b
Matheus Valenzuela <matheuszvalenz@gmail.com>
2024-02-07 12:44:34 +0000

    Added clerk - working on Header component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,19 @@
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+    </ClerkProvider>
+  );
+}
---
+++ components/shared/header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+
+const header = () => {
+  return (
+    <header className="h-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default header;
