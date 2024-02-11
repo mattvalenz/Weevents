ae639f202da06dc539b1f423239e8df15cf5cc6c
Matheus Valenzuela <matheuszvalenz@gmail.com>
2024-02-11 22:13:51 +0000

    completed hero section, started working on backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,6 +23,14 @@ export default function Home() {
           <Image src="/assets/images/hero.png" alt="hero" width={1000} height={1000} className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"/>
         </div>
       </section>
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trusted by <br/> Thousands of Events</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row ">
+          Search 
+          CategoryFilter
+        </div>
+      </section>
     </>
   );
 }
